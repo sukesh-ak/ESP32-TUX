f832b0d51913fb96a9cce77e4968ff19684ca277
zackees <zachvorhies@protonmail.com>
2023-01-03 13:13:49 -0800

    adds passing in --port to deploy.sh/.bat

--- deploy.bat
+++ deploy.bat
@@ -1,2 +1,15 @@
 del .\managed_components\espressif__fmt\.component_hash 2>nul
+@echo off
+REM Look for --port argument and if found then set it as ESPPORT so that idf.py can use it
+:loop
+if "%~1" == "--port" (
+  set "ESPPORT=%~2"
+  shift
+  shift
+) else if not "%~1" == "" (
+  shift
+)
+if not "%~1" == "" goto loop
+:end
+@echo on
 activate.bat && idf.py build && idf.py flash monitor
--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,19 @@
 #! /bin/bash
 set -e
+# Get the --port argument and set it to the ESPPORT so that it
+# can be used by idf.py
+while [[ $# -gt 0 ]]; do
+    key="$1"
+    case $key in
+        --port)
+        # set the env variable ESPPORT environmental variable
+        export ESPPORT="$2"
+        shift # past argument
+        shift # past value
+        ;;
+        *)    # unknown option
+        shift # past argument
+        ;;
+    esac
+done
 . activate.sh && ./build.sh && idf.py flash monitor
